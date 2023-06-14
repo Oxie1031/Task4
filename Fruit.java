9bb4276b050438140940afb8faf095bb349f7e2b
Oxie1031 <takuma.oku.44@gmail.com>
2023-06-14 15:07:03 +0900

    第4回課題提出時の小山さんからのFBを元にList<Fruit>に対するStreamを実装

---
+++ Fruit.java
@@ -0,0 +1,17 @@
+public class Fruit {
+    private String name;
+    private int count;
+
+    public Fruit(String name, int count) {
+        this.name = name;
+        this.count = count;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
